35dc492af716d789ca0ce8ddb59e6bfd86258bdf
Federico Perrone <55171274+FoxtrotPotato@users.noreply.github.com>
2022-12-18 12:00:56 -0300

    arreglos

--- HSM_challenge/HSM_challenge.js
+++ HSM_challenge/HSM_challenge.js
@@ -65,13 +65,13 @@ function processData() {
         var maxPMCantidad = 0;
         var maxDiasPMCant = 0;
         var maxDiasPMMes = 0;
-        var racha = 0;
+        var rachaMax = 0;
         var rachaDia = 0;
         var rachaMes = 0;
         var precipTotal = 0;
         var mesesRep = [];
         var precipMensuales = [];    
-
+        
         for (var i = 0; i < temp[sheetName1].length; i++) {            
             var j = parseInt(temp[sheetName1][0][2]);
             if(isNaN(k)) {
@@ -87,19 +87,37 @@ function processData() {
                 precipitaciones [i] = parseInt(temp[sheetName1][i][2]);
             }
         }
-
-
+        
+        
         var k = 0;
         var mesesAgrupCount = [];
-        var mesAnt = meses[0];
+        var racha = 0;
+        var mesTemp = 0;
         mesesRep[k] = meses[0];
         mesesAgrupCount[k] = 0;
         precipMensuales[k] = 0;
         avgPrecipMensuales[k] = 0;
-        
+        rachaMes = meses[k];
+        rachaDia = dias[k];
+                
         for (var i = 0; i < meses.length; i++) {   
             precipTotal = precipTotal + precipitaciones [i];
-            var q = i - 1;
+                        
+            if (mesTemp == meses[i]) {
+                racha++;
+            } else {
+                racha = 1;
+            }
+            mesTemp = meses[i];
+            
+            if(racha > rachaMax){
+                rachaMax = racha; 
+                if (rachaMes !== meses[i]){
+                    rachaDia = dias[i];
+                }
+                rachaMes = meses[i];
+            }
+            
 
             if (meses[i] !== mesesRep[k]) {
                 k++;
@@ -107,27 +125,21 @@ function processData() {
                 mesesAgrupCount[k] = 0;
                 precipMensuales[k] = 0;
                 avgPrecipMensuales[k] = 0;
-            }
+            } 
 
             mesesAgrupCount[k]++;
             precipMensuales[k] = precipMensuales[k] + precipitaciones [i];
             avgPrecipMensuales[k] = precipMensuales[k] / mesesAgrupCount[k];
-
-            if(meses[q] = meses[i]){
-                racha++;
-                rachaDia = dias[i];
-                rachaMes = mes[i];
-            }
-
+           
         }
         
      
         var strPrecipMensuales = "";
 
         for (var i = 0; i < mesesRep.length; i++) {    
-            var m = mesesRep[i];
-                
-            strPrecipMensuales = strPrecipMensuales + mes[m] + ": " + avgPrecipMensuales[i] + "mm. -   ";
+            var m = mesesRep[i]-1;
+
+            strPrecipMensuales = strPrecipMensuales + "  - " + mes[m] + ": " + avgPrecipMensuales[i] + "mm.";
 
                 if(maxPMCantidad < precipMensuales[i]){
                     maxPMCantidad = precipMensuales[i];
@@ -149,9 +161,13 @@ function processData() {
             document.getElementById("maxPMCantidad_li").innerHTML = maxPMCantidad + "mm.";
             document.getElementById("maxDiasPMMes_li").innerHTML = maxDiasPMMes;
             document.getElementById("maxDiasPMCant_li").innerHTML = maxDiasPMCant + " dias.";
-            document.getElementById("rachaMes_li").innerHTML = rachaMes;
+            
+            document.getElementById("rachaMes_li").innerHTML = mes[rachaMes-1];
             document.getElementById("rachaDia_li").innerHTML = rachaDia;
-            document.getElementById("racha_li").innerHTML = racha + " dias.";
+            document.getElementById("racha_li").innerHTML = rachaMax + " dias.";
     }
-
+    
+    console.log(mesesAgrupCount);
+    console.log(precipMensuales);
+    console.log(avgPrecipMensuales);
 }
